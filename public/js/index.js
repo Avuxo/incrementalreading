5795a039048c85197bc0a1eecccf8949e23757f5
ben <ben@july7.pw>
2017-01-18 13:51:04 -0500

    fix poor naming, and update to accomodate json

--- public/js/index.js
+++ public/js/index.js
@@ -5,20 +5,8 @@ app.controller("navCtrl", function($scope, $http){
     //get the article
     $http({method: 'GET', url: '/article'})
         .then(function(res){
-            $scope.test = res.data; //GET success
+            $scope.title = res.data.title; //GET success
         },function(res){
             console.log('Failure to obtain article.'); //GET failure
         });
 });
-
-
-
-
-
-
-
-
-
-
-
-
