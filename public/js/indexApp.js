3a46dc173d7ead2edb4e611b1994c889e01385ff
ben <ben@july7.pw>
2017-02-03 13:45:13 -0500

    fiddling with http calls on front end

--- public/js/indexApp.js
+++ public/js/indexApp.js
@@ -1,28 +1,30 @@
 var app = angular.module("indexApp", []);
 
 app.controller("navCtrl", function($scope, $http){
-
+    $scope.getArticle = function(){
     //get the article
-    $http({method: 'GET', url: '/article'})
-        .then(function(res){ //GET success
-            $scope.title = res.data.title;
-            $scope.source = res.data.domain;
-        },function(res){ //GET failure
-            console.log('Failure to obtain article.');
-        });
+        $http({method: 'GET', url: '/article'})
+            .then(function(res){ //GET success
+                $scope.title = res.data.title;
+                $scope.source = res.data.domain;
+            },function(res){ //GET failure
+                console.log('Failure to obtain article.');
+            });
+    }
 });
 
 app.controller("articleCtrl", function($scope, $http){
-    $http({method: 'GET', url: '/article'})
-        .then(function(res){ //GET success
-            if(typeof res.data.content != 'undefined'){
-                $scope.articleBody = res.data.content; //this is just LOOKING for XSS
-                document.getElementById('article').innerHTML = $scope.articleBody;
-            }else{
-                console.log("Failed to obtain article, returned 'undefined'");
-            }
-        },function(res){ //GET failure
-            console.log('Failure to obtain article');
-        });
-    
+    $scope.getArticle = function(){
+        $http({method: 'GET', url: '/article'})
+            .then(function(res){ //GET success
+                if(typeof res.data.content != 'undefined'){
+                    $scope.articleBody = res.data.content; //this is just LOOKING for XSS
+                    document.getElementById('article').innerHTML = $scope.articleBody;
+                }else{
+                    console.log("Failed to obtain article, returned 'undefined'");
+                }
+            },function(res){ //GET failure
+                console.log('Failure to obtain article');
+            });
+    }
 });
