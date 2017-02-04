c6482fc2c35ca18101c08588784832bdb1a32a23
ben <ben@july7.pw>
2017-02-04 13:35:31 -0500

    more db work, add in new dependency so the db can get console input

--- db.js
+++ db.js
@@ -4,8 +4,13 @@
 const express = require('express');
 const fs = require('fs');
 const jsonfile = require('jsonfile');
+const readline = require('readline');
 
 const app = express();
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
 
 //filepath for database
 const path = './db/db.json'
@@ -17,11 +22,20 @@ app.listen(8090, function(){
     console.log('App running on port 8090');
 });
 
+//get user input
+rl.on('line', function(input){
+    if(input == "save"){
+        writeData();
+    }
+})
+
+
 /*
   WriteData
   write the version of the db from memory back into db.json
 */
 function writeData(){
+    console.log("writing to db")
     jsonfile.writeFile(path, db, function(err){
         console.log(err);
     });
